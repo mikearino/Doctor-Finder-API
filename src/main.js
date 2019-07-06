6817c5c25c6e46dde73f815fb61e602b70ceed06
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-06 13:54:26 -0700

    add more functionality to append different doctors names/ address etc.

--- src/main.js
+++ src/main.js
@@ -16,15 +16,16 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body.data[0].profile.last_name);//how to target last name
+      console.log(body.data[0].practices[0].visit_address.street)
       for (let i=0; i <= body.data.length; i++) {
         if (body.data[i].profile.last_name.includes(docName) || body.data[i].profile.first_name.includes(docName)) {
           docArray.push(body.data[i]);
           console.log(docArray);
-
+          $(".output").append(`First name: ${body.data[i].profile.first_name}  Last name: ${body.data[i].profile.last_name}
+            Address: ${body.data[0].practices[0].visit_address.street}` );
         }
         let docArray = [];
     }
-      $(".output").text(body.insult);
     }, function(error) {
       $(".output").text(`There was an error! ${error.message}`);
     });
